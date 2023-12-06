b2b6fe48e53b4483244f8c0883851c62a115eac2
Krieg <krieg@Krieg-Macbook-Pro.local>
2023-12-06 15:32:33 -0500

    added email and phone number

--- src/SingleUser.jsx
+++ src/SingleUser.jsx
@@ -2,9 +2,9 @@ function SingleUser({user, onBack}) {
 
 return (
     <>
-        <h3>{user.name}</h3>
-    <h4>{user.email}</h4>
-    <h4>{user.phone}</h4>
+        <h3>Name: {user.name}</h3>
+    <h4>Email: {user.email}</h4>
+    <h4>Phone Number: {user.phone}</h4>
     <button onClick={onBack}> Back to List</button>
 
     </>
